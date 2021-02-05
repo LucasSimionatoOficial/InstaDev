42c06377ffc482581973a4a269d0a4cd1b6a8346
Robert Sena <robert.sena25@gmail.com>
2021-02-05 11:10:22 -0300

    Main edição de perfil passado para a view

---
+++ Controllers/EditarController.cs
@@ -0,0 +1,38 @@
+using System;
+using instadev.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace instadev.Controllers
+{
+    [Route("Editar")]
+    public class EditarController : Controller
+    {
+        Usuario usuarioModel = new Usuario();
+
+        public IActionResult Index()
+        {
+            ViewBag.Usuarios = usuarioModel.ListarUsuarios();
+
+            return View();
+        }
+
+        [Route("{id}")]
+        public IActionResult EditarUsuario(int id, IFormCollection form)
+        {     
+            re
+        }
+
+        [Route("{id}")]
+        public IActionResult DeletarUsuario(int id)
+        {
+            usuarioModel.DeletarUsuario(id);
+
+            ViewBag.Usuarios = usuarioModel.ListarUsuarios();
+
+            return LocalRedirect("~/Editar/Listar");
+        }
+        
+        
+    }
+}
