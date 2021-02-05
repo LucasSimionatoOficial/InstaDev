f9d430cb603416d9f32c5a524c369b71d333897e
Robert Sena <robert.sena25@gmail.com>
2021-02-05 10:22:16 -0300

    Métodos cadastrar no usuarioController iniciado

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,9 +1,11 @@
+using System;
 using instadev.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace instadev.Controllers
 {
+    [Route("Equipe")]
     public class UsuarioController : Controller
     {
         Usuario usuarioModel = new Usuario();
@@ -15,10 +17,33 @@ namespace instadev.Controllers
             return View();
         }
 
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            Usuario novoUsuario = new Usuario();
+
+            novoUsuario.IdUsuario = Int32.Parse( form["IdUsuario"] );
+            novoUsuario.Nome = form["Nome"];
+            novoUsuario.NomeUsuario = form["NomeUsuario"];
+            novoUsuario.Foto = form["Foto"];
+            novoUsuario.DataNascimento = DateTime.Parse( form["DataNascimento"] );
+            novoUsuario.Email = form["Email"];
+            novoUsuario.Username = form["Username"];
+            novoUsuario.Senha = form["Senha"];
+
+            usuarioModel.CadastrarUsuario(novoUsuario);
+            ViewBag.Usuarios = usuarioModel.ListarUsuarios();
+
+            return LocalRedirect("~/Usuario/Listar");
+            
+        }
+
         public IActionResult EditarUsuario(IFormCollection form)
         {
             
+
+            return View();
         }
+        
         
     }
 }
